7ca57b9d3be1789f22286364c44c1f1849c4875f
耿裕健-FE <gengyujian@xiaolian.link>
2023-02-14 17:00:03 +0800

    样式,waas zh 2

--- deploy.sh
+++ deploy.sh
@@ -19,3 +19,5 @@ docker run --rm --name slate -v $(pwd)/build/API-WAPI/zh:/srv/slate/build -v $(p
 docker run --rm --name slate -v $(pwd)/build/API-WAPI/en:/srv/slate/build -v $(pwd)/API-WAPI/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WAPI/en/includes:/srv/slate/source/includes gengyujian/slate build
 
 sudo cp $(pwd)/index.html $(pwd)/build
+
+docker run --rm --name slate -v $(pwd)/build/API-WAPI/en:/srv/slate/build -v $(pwd)/API-WAPI/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WAPI/en/includes:/srv/slate/source/includes gengyujian/slate serve
