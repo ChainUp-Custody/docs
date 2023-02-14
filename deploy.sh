ded244b1a8477b28b9c262d29db84c2cb8433c01
耿裕健-FE <gengyujian@xiaolian.link>
2023-02-14 15:58:07 +0800

    sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 set -o errexit #abort if any command fails
 
-#docker build . -t gengyujian/slate
+#docker pull gengyujian/slate-stages
 
 # build API-MPC/zh
-docker run --rm --name slate -v $(pwd)/build/API-MPC/zh:/srv/slate/build -v $(pwd)/API-MPC/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-MPC/zh/includes:/srv/slate/source/includes gengyujian/slate build
+docker run --rm --name slate -v $(pwd)/build/API-MPC/zh:/srv/slate/build -v $(pwd)/API-MPC/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-MPC/zh/includes:/srv/slate/source/includes gengyujian/slate-stages build
 # build API-MPC/en
-docker run --rm --name slate -v $(pwd)/build/API-MPC/en:/srv/slate/build -v $(pwd)/API-MPC/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-MPC/en/includes:/srv/slate/source/includes gengyujian/slate build
+docker run --rm --name slate -v $(pwd)/build/API-MPC/en:/srv/slate/build -v $(pwd)/API-MPC/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-MPC/en/includes:/srv/slate/source/includes gengyujian/slate-stages build
 
 # build API-WaaS-V2/zh
-docker run --rm --name slate -v $(pwd)/build/API-WaaS-V2/zh:/srv/slate/build -v $(pwd)/API-WaaS-V2/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WaaS-V2/zh/includes:/srv/slate/source/includes gengyujian/slate build
+docker run --rm --name slate -v $(pwd)/build/API-WaaS-V2/zh:/srv/slate/build -v $(pwd)/API-WaaS-V2/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WaaS-V2/zh/includes:/srv/slate/source/includes gengyujian/slate-stages build
 # build API-WaaS-V2/en
-docker run --rm --name slate -v $(pwd)/build/API-WaaS-V2/en:/srv/slate/build -v $(pwd)/API-WaaS-V2/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WaaS-V2/en/includes:/srv/slate/source/includes gengyujian/slate build
+docker run --rm --name slate -v $(pwd)/build/API-WaaS-V2/en:/srv/slate/build -v $(pwd)/API-WaaS-V2/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WaaS-V2/en/includes:/srv/slate/source/includes gengyujian/slate-stages build
 
 # build API-WAPI/zh
-docker run --rm --name slate -v $(pwd)/build/API-WAPI/zh:/srv/slate/build -v $(pwd)/API-WAPI/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WAPI/zh/includes:/srv/slate/source/includes gengyujian/slate build
+docker run --rm --name slate -v $(pwd)/build/API-WAPI/zh:/srv/slate/build -v $(pwd)/API-WAPI/zh/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WAPI/zh/includes:/srv/slate/source/includes gengyujian/slate-stages build
 # build API-WAPI/en
-docker run --rm --name slate -v $(pwd)/build/API-WAPI/en:/srv/slate/build -v $(pwd)/API-WAPI/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WAPI/en/includes:/srv/slate/source/includes gengyujian/slate build
+docker run --rm --name slate -v $(pwd)/build/API-WAPI/en:/srv/slate/build -v $(pwd)/API-WAPI/en/index.html.md.erb:/srv/slate/source/index.html.md.erb -v $(pwd)/API-WAPI/en/includes:/srv/slate/source/includes gengyujian/slate-stages build
 
 #cp $(pwd)/index.html $(pwd)/build
